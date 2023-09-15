0b80709b466d8eff87a27b92b81b513cfd624eb4
Lisa <95229586+LisaKolesnik@users.noreply.github.com>
2023-09-15 10:37:39 +0300

    Delete DogHouse directory

--- DogHouse/script.js
+++
@@ -1,5 +0,0 @@
-let bg = document.querySelector('.bg');
-    window.addEventListener('mousemove', function(e) {
-        let y = e.clientY / window.innerHeight;  
-        bg.style.transform = 'translateY(-' + y * 70 + 'px)';
-    });
